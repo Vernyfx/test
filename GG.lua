1ad9d131d94996d910e0e5a88dd481038fa6c1bf
Vernyfx <111518228+Vernyfx@users.noreply.github.com>
2023-12-21 21:02:20 +0100

    Update GG.lua

--- GG.lua
+++ GG.lua
@@ -614,7 +614,7 @@ function()
             local ConvertedTimer = GetDungeonData("Easy","TimeToOpen"):split("Opens in")[2]:split("s")[1]
         end)
         
-        if GetDungeonData("Easy","Status") == "Opened" and GetDungeonData("Easy","TimeToStart") <= 10 and not game:GetService("Players").LocalPlayer.PlayerGui.Mode.Content.Dungeon.Visible then
+        if GetDungeonData("Easy","Status") == "Opened" and GetDungeonData("Easy","TimeToStart") <= 10 and GetDungeonData("Easy","Mode") ~= "Dungeon" then
             if not _G.InTrial then
 
                 _G.InTrial = true
